ff19652a3710381038e3d5685050508b1c7b5a46
Sid James Smith <ssmith7993@elab-m-03.mchenry.edu>
2016-02-22 11:15:14 -0600

    Initial Commit

---
+++ Family_Tree_Sid_Smith/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Family_Tree_Sid_Smith
+//
+//  Created by Sid James Smith on 2/22/16.
+//  Copyright © 2016 Sid Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
